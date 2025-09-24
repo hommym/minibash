461c0fa4d004e4a22dd824019a28ab4ea77b9cf5
hommym <herbertharthur80@gmail.com>
2025-09-24 12:51:42 +0000

    build: add build script for compiling and linking
    This commit introduces a `run.bash` script to automate the
    compilation and linking process for the Mini-Bash project.
    The script compiles C files in the `src` directory, assembles
    the `minibash.asm` file, and links everything together to
    create the final executable.
    
    The script handles SDL2 and SDL2_ttf dependencies correctly.

---
+++ run.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+
+#cd into src
+cd src
+
+# compile all c file in src to objcet files in build
+
+for file in $(ls *.c)
+do
+        if [ $file == "eventLoop.c" ]; then 
+            gcc -c $file -o ../build/eventLoop.o $(pkg-config --cflags sdl2) 
+
+        elif [ $file == "output.c" ]; then 
+              gcc -c $file -o ../build/output.o $(pkg-config --cflags sdl2) -lSDL2_ttf 
+        fi
+
+done
+
+# going back root dir 
+cd .. 
+
+#cd into utils
+cd utils
+
+#compile all asm files in this dir(N/A)
+
+
+
+# going back root dir 
+cd .. 
+
+#compiling the asm entry point minibash.asm
+yasm -f elf64 -g dwarf2 -l build/main.lst -o build/minibash.o minibash.asm 
+
+
+#cd into build dirr for linking
+cd build
+
+#link everything into a single exe
+gcc minibash.o eventLoop.o output.o -no-pie -o minibash $(sdl2-config --libs) $(pkg-config --libs SDL2_ttf)     
+
+
+# going back root dir 
+cd .. 
